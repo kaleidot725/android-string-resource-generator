4fd51fc55d7930312a7291ef9bf989cb5e483d17
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-05-29 23:21:31 +0900

    Update main.rs

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -6,12 +6,9 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    let content = std::fs::read_to_string(&args.path)?;
+    println!("file conent: {}", content);
+    Ok(())
 }
