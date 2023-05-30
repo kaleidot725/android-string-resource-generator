69b09a722027a99469d34ee422d71126f48d8110
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-05-30 22:07:44 +0900

    Update

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread::sleep, time};
+use log::{info, warn};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -10,5 +12,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)?;
     println!("file conent: {}", content);
+
+    let x = 42;
+    println!("My lucky number is {}.", x);
+
+    let xs = vec![1, 2, 3];
+    println!("The list is: {:?}", xs);
+    
+    let pb = indicatif::ProgressBar::new(100);
+    let time = time::Duration::from_millis(10);
+    for i in 0..100 {
+        sleep(time);
+        pb.inc(1);
+    }
+    pb.finish_with_message("done");
+
+    env_logger::init();
+    info!("starting up");
+    warn!("oops, nothing implemented!");
+
     Ok(())
 }
