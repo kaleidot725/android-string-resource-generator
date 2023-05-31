a38f1a13174876a5bb7d39cd68e2de1f75c1ab89
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-05-31 22:20:09 +0900

    Update

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -10,26 +10,23 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)?;
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!(""))?;
     println!("file conent: {}", content);
-
-    let x = 42;
-    println!("My lucky number is {}.", x);
-
-    let xs = vec![1, 2, 3];
-    println!("The list is: {:?}", xs);
-    
-    let pb = indicatif::ProgressBar::new(100);
-    let time = time::Duration::from_millis(10);
-    for i in 0..100 {
-        sleep(time);
-        pb.inc(1);
-    }
-    pb.finish_with_message("done");
-
-    env_logger::init();
-    info!("starting up");
-    warn!("oops, nothing implemented!");
-
     Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
